b3836df33580fbbf48476e4fb29d6a4b41c3e540
Atwinenickson <atwiinenicxon@gmail.com>
2020-04-11 19:43:40 +0300

    create  authentication app wth react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Route} from "react-router-dom"
+// import Home from "./Home "
+import Profile from "./Profile"
+import Hame from "./Hame"
+import Nav from './Nav'
+import Auth from "./Auth/Auth"
+import Callback from './Callback';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.auth = new Auth(this.props.history)
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <div className="body">
+        {/* <Route path="/" exact component={Home}/> */}
+        <Route
+          path="/"
+          exact
+          render={(props) => <Hame auth={this.auth} {...props} />}
+        />
+
+        <Route
+          path="/callback"
+          render={(props) => <Callback auth={this.auth} {...props} />}
+        />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </>
   );
+}
 }
 
 export default App;
---
+++ src/Callback.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Callback extends Component {
+    componentDidMount() {
+        if (/access_token | id_token | error/.test(this.props.location.hash)) {
+            this.props.auth.handleAuthentication(); 
+        } else {
+            throw new Error("Invalid callback Url");
+        }
+    };
+    render() {
+        return (
+            <h1>
+                loading....
+            </h1>
+        )
+    }
+}

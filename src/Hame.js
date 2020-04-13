39709a6e87d72d1079a09f60435fafa60ed7e5b0
Atwinenickson <atwiinenicxon@gmail.com>
2020-04-13 17:35:39 +0300

    Added the profile data, logging out and signing up to the auth0 app

--- src/Hame.js
+++ src/Hame.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 class Hame extends Component {
-    render() {
-      const {isAuthenticated, login} = this.props.auth
+  render() {
+    const { isAuthenticated, login } = this.props.auth;
     return (
-    <div>
-            <h1>Home</h1>
-            {isAuthenticated()}
-                <Link to="/profile">View Profile</Link> :
+      <div>
+        <h1>Home</h1>
+        {isAuthenticated() ? (
+          <Link to="/profile">View profile</Link>
+        ) : (
             <button onClick={login}>Log In</button>
-    </div>
-    )
+          )}
+      </div>
+    );
   }
 }
 
 export default Hame;
+
